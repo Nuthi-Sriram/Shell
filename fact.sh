0017aac6d34d39647d56ee223065fc608535d392
Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-01-19 16:32:04 +0530

    added fibonacci.sh and shebang

--- fact.sh
+++ fact.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Enter a number:" read n
 f=1
 for((i = 1 ; i <= n ; i++)) do
---
+++ fibonacci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter the number of terms:" read n
+echo "Fibonacci series is:" a=-1
+b=1
+c=0
+for((i = 1 ; i <= n ; i++)) do
+c=`expr $a + $b` echo $c
+a=$b b=$c
+done
