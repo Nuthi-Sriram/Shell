de7596daf79878b146e9dae6926c395ff123233b
Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-02-02 15:45:04 +0530

    fix: added condition to accept only number
    Signed-off-by: Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>

--- Source Codes/factorial.sh
+++ Source Codes/factorial.sh
@@ -3,6 +3,10 @@
 
 echo "Enter a number which you want to find the factorial of"
 read -r n
+while  [[ ! "$n" =~ ^[-+]?[0-9]+$ ]]; do
+	echo "Please enter only numbers :"
+	read -r n
+done
 fact=1
 
 for((i=2;i<=n;i++))
